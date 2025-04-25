117c11acc8ddb4284d792897864bf9ea1b1038a9
mkx8 <mkx8@fresnou.org>
2025-04-25 08:40:22 -0700

    Bleh

--- routes/index.js
+++ routes/index.js
@@ -10,4 +10,6 @@ router.get('/products', productController.viewProducts);
 router.get('/products/profile/:id', productController.renderProfile);
 router.get('/products/edit/:id', productController.renderEditForm);
 router.post('/products/edit/:id', productController.updateProduct);
+router.get('/products/add', productController.renderAddForm);
+router.post('/products/add', productController.addProduct);
 module.exports = router;
